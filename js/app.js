9ffeebb11324a7ca3f77f6227f9b95273e6485b4
elenayatsenko707 <lenasova95@gmil.com>
2021-04-22 17:24:02 +0300

    lesson18

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+
 $(document).ready(function() {
 	// $('body').hide()
+
+const swiper = new Swiper('.main-slider', {
+	// Optional parameters
+	loop: true,
+	autoplay:{
+		dalay:3000,
+	},
+  
+	// If we need pagination
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+  
+	// Navigation arrows
+	navigation: {
+		prevEl: '.swiper__prev',
+	  	nextEl: '.swiper__next',
+	  
+	}
+  
+  });
 })
