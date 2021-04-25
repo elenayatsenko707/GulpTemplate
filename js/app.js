4c270612f404db9d247bbf8a3dd233e9a0dbc14e
elenayatsenko707 <lenasova95@gmil.com>
2021-04-26 01:58:01 +0300

    lesson20

--- js/app.js
+++ js/app.js
@@ -1,25 +1,20 @@
-
 $(document).ready(function() {
-	// $('body').hide()
 
-const swiper = new Swiper('.main-slider', {
-	// Optional parameters
-	loop: true,
-	autoplay:{
-		dalay:3000,
-	},
-  
-	// If we need pagination
-	pagination: {
-	  el: '.swiper-pagination',
-	},
-  
-	// Navigation arrows
-	navigation: {
-		prevEl: '.swiper__prev',
-	  	nextEl: '.swiper__next',
-	  
-	}
-  
-  });
-})
+    const swiper = new Swiper('.main-slider', {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+        },
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.slider__next',
+            prevEl: '.slider__prev',
+        }
+    });
+    $( ".projects__tabs" ).tabs();
+});
